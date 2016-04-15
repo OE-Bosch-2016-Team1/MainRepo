2f0de646b625e3005580a05b9d5a15159636d9fa
zhodvogner@gmail.com <zhodvogner@gmail.com>
2016-04-15 02:19:18 +0200

    !!!Project-architecture changes: usage of root .idea folder and SW/out for root of output classes, used third-party libraries placed into .idea/lib (no external dependencies) workspace.xml excluded in .gitignore (it's a question is yet - might be it will be necessary later) ScenePoint.java correction: fixes when degree of rotation or viewAngle greater than 360 degrees Parking.java correction: fixes of missing topPoint and bottomPoint settings

--- SW/environment/src/hu/nik/project/environment/objects/Parking.java
+++ SW/environment/src/hu/nik/project/environment/objects/Parking.java
@@ -13,14 +13,27 @@ public class Parking extends Road implements Serializable {
 
     public enum ParkingType {
         PARKING_0,
-        PARKING_90,
+        PARKING_90
     }
 
     private ParkingType type;
 
+    private static final int parkingWidth = 74;
+    private static final int parkingLength = 116;
+
     public Parking(ScenePoint basePosition, int rotation, ParkingType type) throws SceneObjectException {
         super(basePosition, rotation);
         this.type = type;
+
+        int halfParkingWith = parkingWidth / 2;
+
+        if (type == ParkingType.PARKING_0) {
+            super.setTopPoint(ScenePoint.rotatePointAroundPoint(basePosition, new ScenePoint(basePosition.getX(), basePosition.getY() + halfParkingWith), rotation));
+            super.setBottomPoint(ScenePoint.rotatePointAroundPoint(basePosition, new ScenePoint(basePosition.getX() + parkingLength, basePosition.getY() + halfParkingWith), rotation));
+        } else { // ParkingType.PARKING_90
+            super.setTopPoint(ScenePoint.rotatePointAroundPoint(basePosition, new ScenePoint(basePosition.getX() + halfParkingWith, basePosition.getY()), rotation));
+            super.setBottomPoint(ScenePoint.rotatePointAroundPoint(basePosition, new ScenePoint(basePosition.getX() + halfParkingWith, basePosition.getY() + parkingLength), rotation));
+        }
     }
 
     public ParkingType getObjectType() {
